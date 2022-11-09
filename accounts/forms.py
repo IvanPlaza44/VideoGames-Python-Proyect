6cec7d5d23be903a113e8f1896f37b34d1d6eb68
Paco71312 <ing.josearjona@gmail.com>
2022-11-09 14:00:18 -0600

    Modificacion al login y agrege cambiar passwory editar perfil de usuario

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,29 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
 class CreacionDeusuario(UserCreationForm):
     username= forms.CharField(label= 'Usuario' ,max_length=20)
     password1=forms.CharField(label='Contraseña:',  widget=forms.PasswordInput)
     password2=forms.CharField(label='Repetir Contraseña:',widget=forms.PasswordInput)
     
+    
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+        help_texts={key:'' for key in fields}
+
+class EditarPerfilDeUsuario(forms.Form):
+    first_name=forms.CharField(label= 'Nombre')
+    last_name=forms.CharField(label= 'Apellido')
+    email=forms.CharField()
+    avatar=forms.ImageField(required=False)
+    
+class CambioDeContraseña(PasswordChangeForm):
+    old_password=forms.CharField(label='Contraseña Anterior:',  widget=forms.PasswordInput)
+    new_password1=forms.CharField(label='Nueva Contraseña:',widget=forms.PasswordInput)
+    new_password2=forms.CharField(label='Repetir Contraseña:',widget=forms.PasswordInput)
+    class Meta:
+        model=User
+        fields=['old_password','new_password1','new_password2']
         help_texts={key:'' for key in fields}
--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login
 from accounts.models import ExtesionUsuario
-from accounts.forms import CreacionDeusuario
+from accounts.forms import CreacionDeusuario,EditarPerfilDeUsuario, CambioDeContraseña
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+
 # Create your views here.
 
-def iniciar_sesion(request):
+def mi_login(request):
     if request.method== 'POST':
         formulario= AuthenticationForm(request,data=request.POST)
         if formulario.is_valid():
@@ -16,7 +19,7 @@ def iniciar_sesion(request):
             return redirect('index')
     else:
         formulario=AuthenticationForm()
-    return render(request, 'accounts/iniciar_sesion.html',{'formulario':formulario})
+    return render(request, 'accounts/login.html',{'formulario':formulario})
 
 def registrar(request):
     if request.method== 'POST':
@@ -32,3 +35,26 @@ def registrar(request):
 @login_required
 def mi_perfil(request):
     return render(request, 'accounts/perfil_del_usuario.html',{})
+
+@login_required
+def editar_perfil(request):
+    
+    if request.method=='POST':
+        formulario= EditarPerfilDeUsuario(request.POST, request.FILES)
+        if formulario.is_valid():
+            data_nueva=formulario.cleaned_data
+            request.user.first_name=data_nueva['first_name']
+            request.user.last_name=data_nueva['last_name']
+            request.user.email=data_nueva['email']
+            request.user.extesionusuario.avatar=data_nueva['avatar']
+            request.user.extesionusuario.save()
+            request.user.save()
+            return redirect('perfil_del_usuario')
+    else:        
+        formulario=EditarPerfilDeUsuario(initial={'first_name':request.user.first_name, 'last_name':request.user.last_name, 'email':request.user.email,'avatar':request.user.extesionusuario.avatar, })
+    return render(request, 'accounts/editar_perfil.html',{'formulario':formulario})
+
+class CambiarLlave(LoginRequiredMixin, PasswordChangeView):
+    template_name='accounts/cambiar_contaseña.html'
+    success_url='/accounts/perfil_del_usuario'
+    form_class=CambioDeContraseña
