ba748a8b748e34c130ff606a94bee18a97488804
Ivan Plaza <plazaivanalt@gmail.com>
2022-11-10 19:42:17 -0300

    RamaNueva

--- games/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from  games import views
-
-urlpatterns = [
-    path('videojuegos/', views.ver_videojuegos, name='ver_videojuegos'),
-    path('videojuegos/crear', views.crear_videojuego, name='crear_videojuego'),
-    path('videojuegos/editar/<int:id>', views.editar_videojuego, name='editar_videojuego'),
-    path('videojuegos/eliminar/<int:id>', views.eliminar_videojuego, name='eliminar_videojuego')
-]
