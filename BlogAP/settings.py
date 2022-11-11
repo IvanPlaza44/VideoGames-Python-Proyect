1b1b24302a18936e8ca3b309e1159cc6988b911e
Paco71312 <ing.josearjona@gmail.com>
2022-11-11 17:42:09 -0600

    agrege login ene la views

--- BlogAP/settings.py
+++ BlogAP/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'home',
     'games',
     'accounts',
-    'ckeditor'
+    'ckeditor',
 ]   
 
 MIDDLEWARE = [
--- games/models.py
+++ games/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ckeditor.fields import  RichTextField
+from ckeditor.fields import  RichTextFieldgit 
 # Create your models here.
 
 class Videojuego(models.Model):
